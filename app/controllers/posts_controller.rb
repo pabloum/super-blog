0286e10ab01b3e6315183693b938f2a31f1a1aff
Pablo <pablo.um00@gmail.com>
2018-10-29 12:46:38 -0500

    Creates CRUD methods in posts_controller and its views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,43 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+    if params[:concept].present?
+      @posts = @posts.where("title LIKE ? OR description LIKE ?", "%#{params[:concept]}%", "%#{params[:concept]}%")
+    end
+  end
+
+  def edit
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(posts_params)
+    if @post.save
+      redirect_to posts_path, notice: "The post was succesfully saved"
+    else
+      redirect_to :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  private
+    def posts_params
+
+    end
 end
