9404fa66805330887fb135690ba0c740a618343b
Pablo <pablo.um00@gmail.com>
2018-10-29 12:32:01 -0500

    Creates migration with columns title and body on Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  title      :string
+#  body       :text
+#
+
 class Post < ApplicationRecord
 end
---
+++ db/migrate/20181029173030_add_columns_to_post.rb
@@ -0,0 +1,6 @@
+class AddColumnsToPost < ActiveRecord::Migration[5.2]
+  def change
+    add_column :posts, :title, :string
+    add_column :posts, :body, :text
+  end
+end
