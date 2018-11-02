12293a7f854ffd53d1ed898882eebca39809abd3
Pablo <pablo.um00@gmail.com>
2018-11-01 19:36:04 -0500

    Hides buttons when no user is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def private_access
+    redirect_to :new_user_session unless signed_in?
+  end
+
+  def public_access
+    redirect_to root_path if signed_in?
+  end
 end
